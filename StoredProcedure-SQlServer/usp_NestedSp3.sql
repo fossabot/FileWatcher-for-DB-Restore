9cb01cfe7d48b89e4513529161313b3dd5636a9c
Avinash Singh <hiroc.singh@gmail.com>
2019-03-07 13:24:48 -0500

    Added TSQL

---
+++ StoredProcedure-SQlServer/usp_NestedSp3.sql
@@ -0,0 +1,80 @@
+USE [SANDBOX]
+;
+
+/****** Object:  StoredProcedure [dbo].[usp_NestedSP3]    Script Date: 3/7/2019 1:23:57 PM ******/
+SET ANSI_NULLS ON
+;
+
+SET QUOTED_IDENTIFIER ON
+;
+
+CREATE PROCEDURE
+[dbo].[usp_NestedSP3]
+@i_TraceOn		TINYINT		= 0
+AS
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON
+	SET ANSI_NULLS ON
+	SET QUOTED_IDENTIFIER ON
+
+	-- Parameters.
+	DECLARE	
+	@time				VARCHAR(14)
+
+	-- Pre Processing.
+	DECLARE
+	@PROC_NAME			VARCHAR(256),
+	@TIME_NOW			DATETIME,
+	-- File handlings.
+	@OutputFile			NVARCHAR(100),    
+	@FilePath			NVARCHAR(100),    
+	@bcpCommand			NVARCHAR(1000),
+	@command			VARCHAR(1000)
+
+	-- Set constants.
+	SET @PROC_NAME			= OBJECT_NAME(@@PROCID)
+	SET @TIME_NOW			= CURRENT_TIMESTAMP
+
+	-- File Output
+	SET @FilePath = 'C:\Users\avinashs\Downloads\'
+	SET @OutputFile = 'test.txt'
+	SET @bcpCommand = @bcpCommand + @FilePath + @OutputFile + ' -c -t, -T -S'+ @@servername
+	
+
+	IF @i_TraceOn <> 0
+	BEGIN
+		SET @time = CONVERT(VARCHAR, CURRENT_TIMESTAMP - @TIME_NOW, 14)
+		PRINT @PROC_NAME + ' - ' + @time 	
+	END
+
+	UPDATE [FN-LN-From-CRM]
+	SET FirstName = 'Avinash',
+	LastName = 'Nested Level 3'
+	WHERE ContactID = 1119545;
+
+	SET @command = 'BCP "SELECT * FROM SANDBOX.dbo.[FN-LN-From-CRM]" queryout "C:\Users\test'
+
+				+ CONVERT(VARCHAR, YEAR(GETDATE())) + RIGHT('00'
+
+						    + CONVERT(VARCHAR, MONTH(GETDATE())),
+
+							2) + RIGHT('00'
+
+                                                           + CONVERT(VARCHAR, DAY(GETDATE())),
+
+							    2)
+
+					+ '.txt" -c -T -t "|" '
+	EXEC xp_cmdshell @command
+
+	IF (COLUMNS_UPDATED()) > 0 
+	BEGIN
+	    SET @time = CONVERT(VARCHAR, CURRENT_TIMESTAMP - @TIME_NOW, 14)
+	    PRINT  @PROC_NAME + ' - ' + @time + 'I am Nested Level 3'
+	END
+
+
+	RETURN
+;
+
---
+++ StoredProcedure-SQlServer/usp_NestetSp1.sql
@@ -0,0 +1,92 @@
+USE [SANDBOX]
+;
+
+/****** Object:  StoredProcedure [dbo].[usp_NestedSP]    Script Date: 3/7/2019 1:22:30 PM ******/
+SET ANSI_NULLS ON
+;
+
+SET QUOTED_IDENTIFIER ON
+;
+
+CREATE PROCEDURE
+[dbo].[usp_NestedSP]
+@i_TraceOn		TINYINT		= 0
+AS
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON
+	SET ANSI_NULLS ON
+	SET QUOTED_IDENTIFIER ON
+
+	-- Parameters.
+	DECLARE	
+	@time				VARCHAR(14)
+
+	-- Pre Processing.
+	DECLARE
+	@PROC_NAME			VARCHAR(256),
+	@TIME_NOW			DATETIME,
+	-- File handlings.
+	@OutputFile			NVARCHAR(100),    
+	@FilePath			NVARCHAR(100),    
+	@bcpCommand			NVARCHAR(1000),
+	@command			VARCHAR(1000)
+
+	-- Set constants.
+	SET @PROC_NAME			= OBJECT_NAME(@@PROCID)
+	SET @TIME_NOW			= CURRENT_TIMESTAMP
+
+	-- File Output
+	SET @FilePath = 'C:\Users\avinashs\Downloads\'
+	SET @OutputFile = 'test.txt'
+	SET @bcpCommand = @bcpCommand + @FilePath + @OutputFile + ' -c -t, -T -S'+ @@servername
+	
+
+	IF @i_TraceOn <> 0
+	BEGIN
+		SET @time = CONVERT(VARCHAR, CURRENT_TIMESTAMP - @TIME_NOW, 14)
+		PRINT @PROC_NAME + ' - ' + @time 	
+	END
+
+	UPDATE [FN-LN-From-CRM]
+	SET FirstName = 'Avinash',
+	LastName = 'Main Level'
+	WHERE ContactID = 1119545;
+
+	IF (COLUMNS_UPDATED()) > 0 
+	BEGIN
+	    SET @time = CONVERT(VARCHAR, CURRENT_TIMESTAMP - @TIME_NOW, 14)
+	    PRINT  @PROC_NAME + ' - ' + @time + 'Table Updated'
+	END
+
+	-- Insert statements for procedure here
+	-- SELECT * FROM [FN-LN-From-CRM];
+	SET @command = 'BCP "SELECT * FROM SANDBOX.dbo.[FN-LN-From-CRM]" queryout "C:\Users\test'
+
+				+ CONVERT(VARCHAR, YEAR(GETDATE())) + RIGHT('00'
+
+						    + CONVERT(VARCHAR, MONTH(GETDATE())),
+
+							2) + RIGHT('00'
+
+                                                           + CONVERT(VARCHAR, DAY(GETDATE())),
+
+							    2)
+
+					+ '.txt" -c -T -t "|" '
+
+	EXEC xp_cmdshell @command
+	EXEC usp_NestedSP2 1;
+	-- SET @bcpCommand = 'bcp "SELECT * FROM [FN-LN-From-CRM] " queryout '
+
+	
+
+	RETURN
+
+	
+
+
+
+
+;
+
